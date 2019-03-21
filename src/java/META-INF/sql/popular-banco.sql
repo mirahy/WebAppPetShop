13fe1222ec135f13b10888934288202306c70f10
Estudante <Estudante@LAB03PC17>
2019-03-20 22:35:14 -0400

    Atualizando aula 20.03.2019

---
+++ src/java/META-INF/sql/popular-banco.sql
@@ -0,0 +1,3 @@
+insert into cliente( nome, rg, cpf, telefone, email, endereco) values ('Daenerys Targaryen', '3.654.987-8', '123.456.789-89', '(67) 99999-8888', 'targaryen@gmail.com', 'Ilha pedra do Dragão, Casa Targaryen');
+insert into cliente( nome, rg, cpf, telefone, email, endereco) values ('Jon Snow', '3.654.987-8', '123.456.789-89', '(67) 99999-8888', 'joaodasneves@gmail.com', 'Castle Black');
+insert into cliente( nome, rg, cpf, telefone, email, endereco) values ('Jaime Lannister', '3.654.987-8', '123.456.789-89', '(67) 99999-8888', 'regicida@gmail.com', 'Rochedo Casterly - Casa Lanister');
